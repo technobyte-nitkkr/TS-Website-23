fac1f3fc5edc3dd3d9973884ecbf1755f71cb2f8
Aniket Bhatia <aniketbhatia28@gmail.com>
2023-01-06 01:07:00 +0530

    added navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import Nav from './navbar/nav'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <h1 className="text-3xl font-bold underline">
-      Hello world!
+      <Nav/>
     </h1>
   )
 }
