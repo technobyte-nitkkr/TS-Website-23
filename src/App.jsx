11527dec9c67e7eaf2959d1119ff9f3c0318ed78
Yash Mittal <mittal.yash12@hotmail.com>
2023-01-06 22:32:39 +0530

    Fix: Merged changes
    Signed-off-by: Yash Mittal <mittal.yash12@hotmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-import Nav from './navbar/nav'
+import { useState } from "react";
+import "./App.css";
+import Nav from "./navbar/nav";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <h1 className="text-3xl font-bold underline">
-      <Nav/>
-    </h1>
-  )
+  return <h1>Hello World</h1>;
 }
 
-export default App
+export default App;
---
+++ src/Compoents/HomePage/HomePage.js
@@ -0,0 +1,13 @@
+import "./HomePage.css";
+
+const HomePage = () => {
+  return (
+    <div>
+      <div>
+        <h1 className="techspardha"> TECHSPARDHA </h1>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
