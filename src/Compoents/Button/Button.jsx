8fe442b921bcb31ee4d074e351e87ba4cc94a450
Yash Mittal <mittal.yash12@hotmail.com>
2023-01-08 23:15:05 +0530

    Feat: Add Button Component
    Signed-off-by: Yash Mittal <mittal.yash12@hotmail.com>

---
+++ src/Compoents/Button/Button.jsx
@@ -0,0 +1,14 @@
+import "./Button.css";
+
+const Button = ({ symbol, btnText, btnWidth, btnHeight }) => {
+  return (
+    <button
+      style={{ width: { btnWidth }, height: { btnHeight } }}
+      className="btnContainer"
+    >
+      <span className="btnTextSymbol"> {symbol} </span> <span> {btnText} </span>
+    </button>
+  );
+};
+
+export default Button;
