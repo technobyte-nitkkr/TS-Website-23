12493ab2b50f7ee88f8aa421d5c80518b257e81b
Yash Mittal <mittal.yash12@hotmail.com>
2023-01-08 23:14:13 +0530

    Assets: Add SVGs
    Signed-off-by: Yash Mittal <mittal.yash12@hotmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
-import { useState } from "react";
 import "./App.css";
-import Nav from "./navbar/nav";
-
+import image from "../assets/background.svg";
+import HomePage from "./Compoents/HomePage/HomePage";
+import Navbar from "./Compoents/Navbar/Navbar";
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <div
+      className="App"
+      style={{
+        backgroundImage: `url(${image})`,
+        height: "100vh",
+        margin: "0px",
+        padding: "0px",
+        overflowY: "scroll",
+        overflowX: "hidden",
+        // "-webkit-scrollbar": "none",
+        scrollbarWidth: "none",
+        msOverflowStyle: "none",
+      }}
+    >
+      <Navbar />
+      <HomePage />
+    </div>
+  );
 }
 
 export default App;
