f3da0d795929c847bd618e594333e8310610e61f
Yash Mittal <mittal.yash12@hotmail.com>
2023-01-08 23:16:29 +0530

    Feat: Add notification item component
    Signed-off-by: Yash Mittal <mittal.yash12@hotmail.com>

---
+++ src/Compoents/Notification/Item/NotificationItem.jsx
@@ -0,0 +1,12 @@
+import Button from "../../Button/Button";
+import "./NotificationItem.css";
+
+const NotificationItem = () => {
+  return (
+    <div className="notificationItemContainer">
+      <p className="notificationItem"> {">>"} 24th - 28th FEBRUARY </p>
+    </div>
+  );
+};
+
+export default NotificationItem;
