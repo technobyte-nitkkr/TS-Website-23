0660cc9739baba8ff428155b68da9b13130d9521
Yash Mittal <mittal.yash12@hotmail.com>
2023-01-08 23:17:18 +0530

    Feat: Add Landing page component
    Signed-off-by: Yash Mittal <mittal.yash12@hotmail.com>

---
+++ src/Compoents/HomePage/HomePage.jsx
@@ -0,0 +1,46 @@
+import Button from "../Button/Button";
+import NotificationItem from "../Notification/Item/NotificationItem";
+import NotificationList from "../Notification/List/NotificationList";
+import "./HomePage.css";
+
+const HomePage = () => {
+  return (
+    <div className="homeContainer">
+      <div className="homeLeftContainer">
+        <div className="titleContainer">
+          <h1 className="techspardha"> TECHSPARDHA </h1>
+          <h3 className="festTheme"> UNRAVELLING DIMENSIONS </h3>
+          <div className="presentedBy">
+            Presented By:{" "}
+            <span className="presenterLogo">
+              {" "}
+              <img className="presenter" src="../../../assets/do.svg" />{" "}
+            </span>
+            <span className="presenterLogo">
+              {" "}
+              <img
+                className="presenter"
+                src="../../../assets/appwrite.svg"
+              />{" "}
+            </span>
+            <span className="presenterLogo">
+              {" "}
+              <img
+                className="presenter"
+                src="../../../assets/docker.svg"
+              />{" "}
+            </span>
+          </div>
+          <NotificationList />
+        </div>
+      </div>
+      <div className="homeRightContainer">
+        <div className="elementContainer">
+          <img src="../../../assets/element.svg" className="elementSVG" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
