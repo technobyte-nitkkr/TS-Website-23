bca004234dc130b6d0d431b3bb0f5208f7065e51
Yash Mittal <mittal.yash12@hotmail.com>
2023-01-10 22:50:06 +0530

    Draft changes to be merged
    Signed-off-by: Yash Mittal <mittal.yash12@hotmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,30 @@
 import "./App.css";
-import image from "/assets/background.svg";
-import HomePage from "./Compoents/HomePage/HomePage";
-import Navbar from "./Compoents/Navbar/Navbar";
+import image from "/assets/Main-background.svg";
+import HomePage from "./Components/HomePage/HomePage";
+import Navbar from "./Components/Navbar/Navbar";
+import Homecontainer from "./Components/Homecontainer/Homecontainer";
+
 function App() {
   return (
     <div
       className="App"
       style={{
         backgroundImage: `url(${image})`,
+        backgroundColor: "#000000",
         height: "100vh",
         margin: "0px",
         padding: "0px",
         overflowY: "scroll",
         overflowX: "hidden",
-        // "-webkit-scrollbar": "none",
         scrollbarWidth: "none",
         msOverflowStyle: "none",
       }}
     >
-      <Navbar />
+      <div>
+        <Navbar />
+      </div>
       <HomePage />
+      <Homecontainer />
     </div>
   );
 }
--- src/Components/Button/Button.jsx
+++ src/Components/Button/Button.jsx
@@ -3,7 +3,7 @@ import "./Button.css";
 const Button = ({ symbol, btnText, btnWidth, btnHeight }) => {
   return (
     <button
-      style={{ width: { btnWidth }, height: { btnHeight } }}
+      // style={{ width: { btnWidth }, height: { btnHeight } }}
       className="btnContainer"
     >
       <span className="btnTextSymbol"> {symbol} </span> <span> {btnText} </span>
