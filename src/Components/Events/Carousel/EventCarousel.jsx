0ff73a7ce26beb07ea81b22210033ef1341273dc
Shruti30Goel <89864305+Shruti30Goel@users.noreply.github.com>
2023-01-12 20:48:04 +0530

    Events reuploaded

---
+++ src/Components/Events/Carousel/EventCarousel.jsx
@@ -0,0 +1,23 @@
+import EventCard from "../Card/EventCard";
+import papyrusVitaeEventImage from "../../../../public/assets/events/papyrus-vitae.png"
+import programmingEventImage from "../../../../public/assets/events/programming.png"
+import "./EventCarousel.css";
+import Button from "../../Button/Button";
+
+const EventCarousel = () => {
+  return (
+    <div>
+        <div className="carouselCardContainer">
+            <EventCard eventImage={papyrusVitaeEventImage} eventTitle="Papyrus Vitae"/>
+            <EventCard eventImage={programmingEventImage} eventTitle="Programming"/>
+            <EventCard eventImage={papyrusVitaeEventImage} eventTitle="Papyrus Vitae"/>
+            <EventCard eventImage={programmingEventImage} eventTitle="Programming"/>
+        </div>
+        <div className="buttonContainer">
+            <Button btnText="View them all" />
+        </div>
+    </div>
+  );
+};
+
+export default EventCarousel;
