38e1e609123c2df0fa5a2e399f7344f38674c365
Yash Mittal <mittal.yash12@hotmail.com>
2023-01-18 19:20:07 +0530

    Feat: Add Popups
    Signed-off-by: Yash Mittal <mittal.yash12@hotmail.com>

--- src/Components/Events/Carousel/EventCarousel.jsx
+++ src/Components/Events/Carousel/EventCarousel.jsx
@@ -1,10 +1,21 @@
 import EventCard from "../Card/EventCard";
+import Event from "../EventDetail/Event";
 import papyrusVitaeEventImage from "../../../../public/assets/events/papyrus-vitae.png";
 import programmingEventImage from "../../../../public/assets/events/programming.png";
 import "./EventCarousel.css";
 import Button from "../../Button/Button";
+import Overlay from "react-overlay-component";
+
+import { useState } from "react";
 
 const EventCarousel = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const closeOverlay = () => setIsOpen(false);
+  const configs = {
+    animate: true,
+  };
+
   return (
     <div id="eventElement">
       <div className="carouselCardContainer">
@@ -25,9 +36,18 @@ const EventCarousel = () => {
           eventTitle="Programming"
         />
       </div>
-      <div className="buttonContainer" id="glHelper">
+      <div
+        className="buttonContainer"
+        id="glHelper"
+        onClick={() => {
+          setIsOpen(true);
+        }}
+      >
         <Button btnText="View them all" />
       </div>
+      <Overlay configs={configs} isOpen={isOpen} closeOverlay={closeOverlay}>
+        <Event />
+      </Overlay>
     </div>
   );
 };
